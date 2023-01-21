1b2113705b88bdc4cb3da5bf1d714d502fffbdc3
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-01-21 14:21:36 +0500

    dockarized client service & added sign-up and sign-in pages

---
+++ client/src/pages/apple.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Apple = () => {
+  return <h1>Apple page 10</h1>;
+};
+
+export default Apple;
---
+++ client/theme.tsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    grey: {
+      50: "#f0f0f0", // manually adjusted
+      100: "#e0e0e0",
+      200: "#c2c2c2",
+      300: "#a3a3a3",
+      400: "#858585",
+      500: "#666666",
+      600: "#525252",
+      700: "#3d3d3d",
+      800: "#292929",
+      900: "#141414",
+    },
+  },
+});
