9ffeee7ec457760b9f1c78adc710e405db055a52
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-17 21:49:40 +0500

    initialized nats

---
+++ nats-test/src/pub.ts
@@ -0,0 +1,19 @@
+import nats from "node-nats-streaming";
+
+const stan = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("Publisher connected to NATS");
+
+  const data = JSON.stringify({
+    id: "1",
+    product: "white T-shirt",
+    price: 20,
+  });
+
+  stan.publish("product:created", data, () => {
+    console.log("Event published");
+  });
+});
