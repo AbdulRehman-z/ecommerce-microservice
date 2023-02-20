0c2eaa1f5c6ab7c263f8657b65e654363d6f4215
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-20 13:52:24 +0500

    added create product logic + fixed some bugs

--- shop/src/routes/create-product-route.ts
+++ shop/src/routes/create-product-route.ts
@@ -1,14 +1,29 @@
-import { requireAuthMiddleware } from "@abdulrehmanz/common";
+import { requireAuthMiddleware, validateRequest } from "@abdulrehmanz/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { Product } from "../models/product.model";
 
 const router = express.Router();
 
 router.post(
   "/api/products",
   requireAuthMiddleware,
+  [
+    body("title").not().isEmpty().withMessage("Title is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price must be greater than 0"),
+  ],
+  validateRequest,
   (req: Request, res: Response) => {
-    console.log("hello");
-    return res.sendStatus(200);
+    const { title, price } = req.body;
+    const product = Product.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    product.save();
+    return res.sendStatus(201).json(product);
   }
 );
 export { router as createProductRouter };
