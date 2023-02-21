459ede366ae6dacfad6618db7ec803c6220709f6
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-02-21 21:24:33 +0500

    created an instance of subscriber

--- nats-test/src/sub.ts
+++ nats-test/src/sub.ts
@@ -1,17 +1,26 @@
 import nats from "node-nats-streaming";
 import { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Subscriber connected to NATS");
-  const subscription = stan.subscribe("ticket:created");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "order-serice-queue-group"
+  );
 
   subscription.on("message", (msg: Message) => {
-    console.log("Message received");
+    const data = msg.getData();
+
+    console.log(`Event #${msg.getSequence()}\n. Payload: ${data}`);
+    console.log(
+      "---------------------------------------------------------------------"
+    );
   });
 });
