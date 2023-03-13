bf8ac1d71ab32f381190c7b2764ece4acd9a5b6c
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-03-13 22:10:38 +0500

    Updates

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -1,10 +1,11 @@
 import { Subjects } from "./subjects";
+import { OrderStatus } from "./types/order-status";
 
 interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
-    status: string;
+    status: OrderStatus;
     userId: string;
     version: number;
     expiresAt: string;
