ce8bb8cc8ec407cd1b3275d45d8da885fc4f74da
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-03-16 20:04:52 +0500

    wrote some comments & impl safepay payment gateway

--- payment/src/routes/do-payment-route.ts
+++ payment/src/routes/do-payment-route.ts
@@ -8,6 +8,7 @@ import {
 import express, { Response, Request } from "express";
 import { body } from "express-validator";
 import { Order } from "../models/order.model";
+import { safepay } from "../safepay";
 
 const router = express.Router();
 
@@ -20,21 +21,30 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { token, orderId } = req.body;
-
+    const { orderId } = req.body;
+    // find order by id
     const order = await Order.findById(orderId);
 
+    // if order is not found
     if (!order) {
       throw new NotFoundError("Order not found");
     }
 
+    // if order is not belong to user
     if (order.userId !== req.currentUser!.id) {
       throw new BadRequestError("Not authorized");
     }
 
+    // if order is cancelled
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError("Order is cancelled");
     }
+
+    // create payment
+    const { token } = safepay.payments.create({
+      amount: order.price * 100,
+      currency: "USD",
+    });
 
     res.status(201).send({ success: true });
   }
